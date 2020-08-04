88fa1a58962a03bc02914db9289b0e2ce64aa310
Wanying Kwok <kwyfarrah@gmail.com>
2020-08-04 22:27:28 +0900

    migrate some tables and seed

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_04_124648) do
+ActiveRecord::Schema.define(version: 2020_08_04_132437) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -91,7 +91,7 @@ ActiveRecord::Schema.define(version: 2020_08_04_124648) do
     t.bigint "position_id", null: false
     t.integer "weight"
     t.integer "minimum_score", default: 0
-    t.integer "skill_name"
+    t.string "skill_name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "json_name"
