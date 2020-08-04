6143cdba6741905554d30a41fe367ee43ee76957
Wanying Kwok <kwyfarrah@gmail.com>
2020-08-04 22:31:01 +0900

    Api

--- app/jobs/analysis_video_job.rb
+++ app/jobs/analysis_video_job.rb
@@ -1,1 +1,15 @@
-analysis_video_job.rb
+class AnalysisVideoJob < ApplicationJob
+  def perform(job_application_id)
+    job_application = JobApplication.find(job_application_id)
+    return  if job_application.video.attached?
+
+    video = SpeechToText.new
+    transcript = video.video_transcript(cloudinary_url("#{job_application.video.key}.mp3", :resource_type=>"video"))
+    job_application.videotranscript = transcript
+
+    applicant = PersonalityInsightsService.new
+    answer = applicant.profile(job_application.videotranscript)
+    job_application.video_result = answer
+    job_application.save!
+  end
+end
