ef0277e5f88ad703071c8b135f6b5ff48484783d
Wanying Kwok <kwyfarrah@gmail.com>
2020-08-06 19:51:36 +0900

    Quick fix of demo video

--- db/seeds.rb
+++ db/seeds.rb
@@ -159,7 +159,7 @@ application.videotranscript = File.read(Rails.root.join('lib','seeds','video_tra
 json = File.read(Rails.root.join('lib','seeds','personality.json'))
 # result = JSON.parse(json)
 # application.video_result  == result
-application.video.attach(io: File.open('public/video.mp4'), filename: 'interview.mp4')
+application.video.attach(io: File.open('public/demo2.mp4'), filename: 'interview.mp4')
 
 #json = ActiveSupport::JSON.decode(File.read(Rails.root.join('lib','seeds','personality.json')))
 #json = json[0]
