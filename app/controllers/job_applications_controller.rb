f3feca9090b83e3f5e8d42f65aa93cdbaabb6fa2
Wanying Kwok <kwyfarrah@gmail.com>
2020-08-06 19:46:26 +0900

    Quick fix for Sytnax error of controller

--- app/controllers/job_applications_controller.rb
+++ app/controllers/job_applications_controller.rb
@@ -18,13 +18,6 @@ class JobApplicationsController < ApplicationController
     else
       render :edit
     end
-
-  def show
-    extract_skill = ExtractSkill.new
-    cal_score = CalScore.new
-    required_skills = extract_skill.get_required_skills(@job_application)
-    @skill_names_array = extract_skill.extract_requirement_skills(@job_application, required_skills)
-    @overall_score = cal_score.cal_overall_score(@skill_names_array, required_skills)
   end
 
   def show
