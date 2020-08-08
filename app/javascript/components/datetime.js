45e1ff0acb37ebf144b420469378c8a34e07a6ba
Wanying Kwok <kwyfarrah@gmail.com>
2020-08-08 12:27:13 +0900

    check if there is conflict

--- app/javascript/components/datetime.js
+++ app/javascript/components/datetime.js
@@ -1,6 +1,6 @@
 const getCurrentDate = () => {
   var today = new Date().toDateString();
-  const date = document.getElementById('dtText')
+  const date = document.getElementById('dtText');
   if (date) {
   date.innerHTML=today;
   }
