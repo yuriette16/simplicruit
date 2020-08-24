119a505b3c6903d0161f1822c0aa3ee7d09adf8a
Wanying Kwok <kwyfarrah@gmail.com>
2020-08-25 03:17:56 +0900

    change the video_transcript

--- app/jobs/upload_video_transcript_job.rb
+++ app/jobs/upload_video_transcript_job.rb
@@ -1,7 +1,7 @@
 class UploadVideoTranscriptJob < ApplicationJob
   def perform(job_application)
     puts "Add the video_transcript"
-    job_application.videotranscript = File.read(Rails.root.join('lib','seeds','video_transcript.txt'))
+    job_application.videotranscript = File.read(Rails.root.join('lib','seeds','final_video_transcript.txt'))
     job_application.status = 0
     job_application.save!
     puts "Done! "
